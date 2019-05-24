0630f5d1ed76b914ca4fa5e6ea057df4b8f60707
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-05-24 15:14:15 +0200

    init project

---
+++ back/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+
+const routes = require("./routes/index")
+
+const app = express()
+const port = 3000
+
+app.use(morgan("dev"))
+app.use(morgan(":method :url :status :res[content-length] - :response-time "))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.listen(port, console.log(`http://localhost:${port}`))
