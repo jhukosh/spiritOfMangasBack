c7d662d46d301cd4dacaf1120d705af7640629f6
Pierre Vincent <pierre.vincs@gmail.com>
2019-05-25 18:58:14 +0200

    WIP added fetch data en modify existing one

--- back/routes/users.js
+++ back/routes/users.js
@@ -59,4 +59,65 @@ router.delete("/delete-profile", (req, res) => {
 
 })
 
+// Fetch data of all users in UsersDB
+
+router.get("/display-allProfile", (req, res) => {
+
+  connexion.query('SELECT * FROM Users', (err, results) => {
+
+    if (err) {
+
+      console.log(err);
+      res.status(500).send("Erreur lors de l'affichage de tous les utilisateurs");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+
+})
+
+// Fetch data by ID of one user in UsersDB
+
+router.get("/display-profile/:id", (req, res) => {
+
+  const userId = req.body.id
+
+  connexion.query('SELECT * FROM Users WHERE id=' + userId, (err, results) => {
+
+    if (err) {
+
+      console.log(err);
+      res.status(500).send("Erreur lors de l'affichage d'un utilisateur");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+
+})
+
+// Change pseudo of an user in UsersDB
+// IMPORTANT : new value MUST in req must be push with the '' to match MYSQL syntax
+// PUT method is better than POST when working with existing value
+
+router.put("/change-profile", (req, res) => {
+
+  const userId = req.body.id
+  const newUserPseudo = req.body.pseudo
+
+  connexion.query('UPDATE Users SET pseudo ='+newUserPseudo + ' WHERE id='+userId, (err, results) => {
+
+    if (err) {
+
+      console.log(err);
+      res.status(500).send("Erreur lors de la modification de données");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+
+})
+
 module.exports = router
