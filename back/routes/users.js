e4b19af33567777754049d7960575ec9e6f325b5
Pierre Vincent <pierre.vincs@gmail.com>
2019-05-25 17:24:34 +0200

    cleaner code and commentary

--- back/routes/users.js
+++ back/routes/users.js
@@ -12,48 +12,47 @@ router.use(bodyParser.urlencoded({
 
 router.use(bodyParser.json());
 
-// Query
-// Post into UsersDB
+// *******************************************
+// ****************** Query ******************
+// *******************************************
+
+// Post into UsersDB, creating new user
 
 router.post("/create-profile", (req, res) => {
 
   const userData = req.body;
   
-  // connexion à la base de données, et insertion de l'employé
   connexion.query('INSERT INTO Users SET ?', userData, (err, results) => {
 
 
     if (err) {
-      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+
       console.log(err);
       res.status(500).send("Erreur lors de la création d'un utilisateur");
     } else {
-      // Si tout s'est bien passé, on envoie un statut "ok".
-      console.log('Hey je suis un console log!',results)
+
       res.sendStatus(200);
     }
   });
 
 })
 
-// Delete in UsersDB
+// Delete an user in UsersDB
 
 router.delete("/delete-profile", (req, res) => {
 
   const userId = req.query.id
   console.log(userId)
-  
-  // connexion à la base de données, et insertion de l'employé
+
   connexion.query('DELETE FROM Users WHERE id=' + userId, (err, results) => {
 
 
     if (err) {
-      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+
       console.log(err);
       res.status(500).send("Erreur lors de la création d'un utilisateur");
     } else {
-      // Si tout s'est bien passé, on envoie un statut "ok".
-      console.log('Hey je suis un console log!',results)
+
       res.sendStatus(200);
     }
   });
