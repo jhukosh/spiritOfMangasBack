b243d0806a0a20060f0d516e8ac99848c2bec44f
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-05-27 11:26:49 +0200

    adding the genres.js file

---
+++ back/routes/genres.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const router = express.Router()
+const connexion = require('../conf');
+
+// Body parser module
+
+const bodyParser = require('body-parser');
+// Support JSON-encoded bodies
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+router.use(bodyParser.json());
+
+// *******************************************
+// ****************** Query ******************
+// *******************************************
+
+// Post into GenreDB, creating new genre
+
+// Delete a genre in genreDB
+
+// Select All genres 
+
+// Select a genre by name  
+
+// Change name of an genre in GenresDB
