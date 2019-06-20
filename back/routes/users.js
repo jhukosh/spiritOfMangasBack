c4908a3f4f71c248d28cdc8e169cad222dd7cdb4
Julia Hu <juliahukoshi@gmail.com>
2019-06-20 11:42:00 +0200

    wip auth

--- back/routes/users.js
+++ back/routes/users.js
@@ -31,6 +31,23 @@ const verifToken = req => {
   return null;
 };
 
+/* function from tuto 
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+
+  if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1]; 
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+*/
+
 // *******************************************
 // ****************** Query ******************
 // *******************************************
