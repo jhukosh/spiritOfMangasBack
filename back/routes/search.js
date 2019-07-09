375766686a9fc2b1b33a7053f87b44c46431ef86
Julia Hu <juliahukoshi@gmail.com>
2019-07-09 17:42:15 +0200

    search bar in progress

--- back/routes/search.js
+++ back/routes/search.js
@@ -32,4 +32,26 @@ router.get("/filter-genres/:id", (req, res) => {
     })
 })
 
+router.get("/filter-types/:id", (req, res) => {
+
+    const typeId = req.params.id
+    console.log(typeId)
+
+    connexion.query(`SELECT mangas.id, mangas.photoCover, mangas.title, mangas.tome 
+                    FROM types
+                    JOIN series 
+                    ON types.id = series.types_id
+                    JOIN mangas
+                    ON mangas.series_id=series.id
+                    WHERE types_id = ${typeId}`, (err, results) => {
+        if (err) {
+            console.log(err)
+            res.status(500)
+        } else {
+            console.log(results)
+            res.status(200).json(results)
+        }
+    })
+})
+
 module.exports = router
