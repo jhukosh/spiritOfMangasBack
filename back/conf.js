e5291d02dbd474f2e3d18df103f74d60a7908b46
Pierre Vincent <47319615+GitPAV@users.noreply.github.com>
2019-11-15 12:15:57 +0100

    Update conf.js

--- back/conf.js
+++ back/conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connexion = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 user :  'root', // le nom d'utilisateur
-password :  'Symetry1988', // le mot de passe
+password :  '', // le mot de passe
 database :  'spiritofmanga', // le nom de la base de données
 });
 module.exports = connexion;
